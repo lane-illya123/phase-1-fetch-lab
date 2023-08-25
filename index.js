8472772b82a9b2f029e7fb4ddfe73e35d1315ae3
illyalane <lane_illya@yahoo.com>
2023-08-25 06:47:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
   
 }
 
@@ -14,4 +17,7 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
+
+
